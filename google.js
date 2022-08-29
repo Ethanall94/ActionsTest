a5ca615084be3176c101ef0d46fb7a09242425ab
Ethanall94 <85244599+Ethanall94@users.noreply.github.com>
2022-08-29 23:29:06 +0900

    Update google.js

--- google.js
+++ google.js
@@ -1,16 +1,19 @@
 const {GoogleSpreadsheet} = require("google-spreadsheet");
 
-const creds = require("./jsmap.json");
-const mapPaging = require('./map_paging.json');
+const mapPaging = require('./map.json');
+const {GOOGLE_SHEET_ID, CLIENT_EMAIL, PRIVATE_KEY} = process.env;
 
 (async () => {
-  const doc = new GoogleSpreadsheet("1OLMn1NTl8aB9UZ4cPgevfRPnM9YRBsKLENhbgczO1MM");
-  await doc.useServiceAccountAuth(creds);
+  const doc = new GoogleSpreadsheet(GOOGLE_SHEET_ID);
+
+  await doc.useServiceAccountAuth({
+    client_email: CLIENT_EMAIL,
+    private_key: PRIVATE_KEY
+  });
  
   await doc.loadInfo();
 
   const sheet = doc.sheetsByIndex[0];
 
   await sheet.addRows(mapPaging.total);
-  console.log(mapPaging.total);
 })();
